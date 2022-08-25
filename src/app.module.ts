0128387c5e636c1c9993daec464f92cb9ef7ff62
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-25 19:14:59 +0200

    TASK_2 Seed enpoint impemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CharacterModule } from './character/character.module';
 import { ConfigModule } from '@nestjs/config';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
     CharacterModule,
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(`mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD}@ufounders-db.d72zqjx.mongodb.net/?retryWrites=true&w=majority`)
+    MongooseModule.forRoot(`mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD}@ufounders-db.d72zqjx.mongodb.net/?retryWrites=true&w=majority`),
+    SeedModule
   ],
 })
 export class AppModule { }
