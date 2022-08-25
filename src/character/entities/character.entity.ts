58d8f5f55b524d8dd6bb2bc44ad49d74453c188b
Mdelacruzmelo <mdelacruzmelo@gmail.com>
2022-08-25 18:41:03 +0200

    TASK_2 class-validator class-transformer added

--- src/character/entities/character.entity.ts
+++ src/character/entities/character.entity.ts
@@ -1,1 +1,59 @@
-export class Character {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import { Gender } from "src/data/data";
+
+@Schema()
+export class Character extends Document {
+
+    @Prop({ unique: true, index: true })
+    name: string;
+
+    @Prop()
+    height: string;
+
+    @Prop()
+    mass: string;
+
+    @Prop()
+    hair_color: string;
+
+    @Prop()
+    skin_color: string;
+
+    @Prop()
+    eye_color: string;
+
+    @Prop()
+    birth_year: string;
+
+    @Prop()
+    gender: Gender;
+
+    @Prop()
+    homeworld: string;
+
+    @Prop()
+    films: string[];
+
+    @Prop()
+    species: string[];
+
+    @Prop()
+    vehicles: string[];
+
+    @Prop()
+    starships: string[];
+
+    @Prop()
+    created: string;
+
+    @Prop()
+    edited: string;
+
+    @Prop()
+    url: string;
+
+
+}
+
+export const CharacterSchema = SchemaFactory.createForClass(Character)
